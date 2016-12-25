e8a7cfc8b4b050385282f878b96addf2e4d31d20
Cherie Shedd <cherie.shedd@gmail.com>
2016-12-24 23:29:50 -0600

    type write change

--- public/js/script_index.js
+++ public/js/script_index.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["Welcome to my portfolio."],
+            strings: ["Nice to meet you." , "Welcome to my portfolio."],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
